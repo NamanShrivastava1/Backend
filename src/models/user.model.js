ac1c75cb6b0f77f7e3e2978071133016a3057a24
Naman Shrivastava <namanshrivastava008@gmail.com>
2026-05-17 10:57:24 +0530

    style: format user model code for consistency and readability

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,90 +1,90 @@
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 
-import cafeModel from "./cafe.model.js";
-import menuModel from "./menu.model.js";
+import cafeModel from './cafe.model.js';
+import menuModel from './menu.model.js';
 
 const userSchema = new mongoose.Schema({
-    fullname: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-    },
-    mobile: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-        select: false,
-    },
-    jwtVersion: {
-        type: Number,
-        default: 0,
-    },
-    isVerified: {
-        type: Boolean, 
-        default: false
-    },
-    otp: {
-        type: String
-    },
-    otpExpiry: {
-        type: Date
-    }
-})
+  fullname: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    select: false,
+  },
+  jwtVersion: {
+    type: Number,
+    default: 0,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  otp: {
+    type: String,
+  },
+  otpExpiry: {
+    type: Date,
+  },
+});
 
 userSchema.methods.generateAuthToken = function () {
-    const token = jwt.sign(
-        {
-            _id: this._id,
-            jwtVersion: this.jwtVersion,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: "7d" }
-    );
-    return token;
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      jwtVersion: this.jwtVersion,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: '7d' }
+  );
+  return token;
 };
 
 userSchema.methods.comparePassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 userSchema.statics.hashPassword = async function (password) {
-    return await bcrypt.hash(password, 10);
-}
+  return await bcrypt.hash(password, 10);
+};
 
 userSchema.pre('findOneAndDelete', async function (next) {
-    const user = await this.model.findOne(this.getFilter());
-    if (!user) return next();
+  const user = await this.model.findOne(this.getFilter());
+  if (!user) return next();
 
-    // Delete all cafes belonging to this user
-    const cafes = await cafeModel.find({ user: user._id });
+  // Delete all cafes belonging to this user
+  const cafes = await cafeModel.find({ user: user._id });
 
-    for (const cafe of cafes) {
-        // Delete menu items for each cafe
-        await menuModel.deleteMany({ cafe: cafe._id });
-    }
+  for (const cafe of cafes) {
+    // Delete menu items for each cafe
+    await menuModel.deleteMany({ cafe: cafe._id });
+  }
 
-    // Delete all cafes after deleting their menus
-    await cafeModel.deleteMany({ user: user._id });
+  // Delete all cafes after deleting their menus
+  await cafeModel.deleteMany({ user: user._id });
 
-    next();
+  next();
 });
 
-const user = mongoose.model("user", userSchema);
+const user = mongoose.model('user', userSchema);
 
 export default user;
